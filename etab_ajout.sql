11481f9158004a7e2bbc29b5cb99e10d7dcc6b2f
DSMejantel <149260856+DSMejantel@users.noreply.github.com>
2024-05-20 22:03:12 +0200

    geographic search correction

--- etab_ajout.sql
+++ etab_ajout.sql
@@ -52,47 +52,68 @@ select
     
     -- Enregistrer la notification dans la base
  INSERT INTO etab(type, nom_etab, description, Lat, Lon) SELECT $type, $nom_etab, $description, $Lat, $Lon WHERE $description IS NOT NULL;
- 
- -- Fiches des établissments
- SELECT 'card' as component,
-  'Établissements' AS title,
-   'Liste des écoles, collèges et lycées du PIAL' AS description;
-SELECT 
-  type  AS description,
-  'green' as color,
-  description as footer,
-  'building-community' as icon,
-  nom_etab AS title,
-  'etab_notif.sql?id=' || id as link
-FROM etab;
 
--- Nouvel établissement    
+-- Onglets
+SET tab=coalesce($tab,1);
+select 'tab' as component;
+select  'Ajouter depuis une carte'  as title, 'world' as icon, 1  as active, CASE WHEN $tab=1 THEN 'orange' ELSE 'green' END as color, 'etab_ajout.sql?tab=1' as link;
+select  'Ajouter depuis un formulaire' as title, 'forms' as icon, 1 as active, CASE WHEN $tab=2 THEN 'orange' ELSE 'green' END as color, 'etab_ajout.sql?tab=2' as link; 
+
+-- Nouvel établissement   
+select 
+    'divider' as component,
+    'Ajouter depuis une carte'   as contents
+    WHERE $tab=1;
+      
+select 
+    'form' as component,
+    'etab_ajout_carte.sql' as action,
+    'etab_ajout_carte' as id,
+    ''  as validate
+    WHERE $tab=1;
+select 'user_search' as name, 'Ville ou adresse' as label
+    WHERE $tab=1; 
+   
+select 
+    'button' as component,
+    'center' as justify
+        WHERE $tab=1;    
+select 
+    'etab_ajout_carte' as form,
+    'etab_ajout_carte.sql' as link,
+    'orange'    as outline,
+    'world' as icon,
+    'Rechercher'  as title
+    WHERE $tab=1; 
+
+select 
+    'divider' as component,
+    'Ajouter depuis un formulaire'   as contents
+    WHERE $tab=2; 
 SELECT 
     'form' as component,
     'Nouvel établissement' as title,
     'etab_ajout' as id,
     ''     as validate,
-    'Recommencer' as reset;
+    'Recommencer' as reset
+    WHERE $tab=2;
     
-    SELECT 'Catégorie' AS label, 'type' AS name, 6 as width, 'select' as type, 1 as value, TRUE as searchable, '[{"label": "---", "value": "---"}, {"label": "École", "value": "école"}, {"label": "Collège", "value": "Collège"}, {"label": "Lycée", "value": "Lycée"}]' as options;
-    SELECT 'Établissement scolaire' AS label, 'nom_etab' AS name, 6 as width, $search as value, TRUE as required;
-    SELECT 'Adresse' AS label, 'description' AS name;
-    SELECT 'Latitude' AS label, 'Lat' AS name, 6 as width, $lat as value;
-    SELECT 'Longitude' AS label, 'Lon' AS name, 6 as width, $lon as value;
+    SELECT 'Catégorie' AS label, 'type' AS name, 6 as width, 'select' as type, 1 as value, TRUE as searchable, '[{"label": "---", "value": "---"}, {"label": "École", "value": "école"}, {"label": "Collège", "value": "Collège"}, {"label": "Lycée", "value": "Lycée"}]' as options WHERE $tab=2;
+    SELECT 'Établissement scolaire' AS label, 'nom_etab' AS name, 6 as width, $search as value, TRUE as required WHERE $tab=2;
+    SELECT 'Adresse' AS label, 'description' AS name WHERE $tab=2;
+    SELECT 'Latitude' AS label, 'Lat' AS name, 6 as width, $lat as value WHERE $tab=2;
+    SELECT 'Longitude' AS label, 'Lon' AS name, 6 as width, $lon as value WHERE $tab=2;
 
 select 
-    'button' as component;    
+    'button' as component
+    WHERE $tab=2;   
 select 
     'etab_ajout'         as form,
     'green'      as color,
     'square-plus' as icon,
-    'Ajouter'         as title;
-select 
-    'etab_ajout_carte.sql' as link,
-    'etab_ajout'            as form,
-    'orange'          as outline,
-    'world' as icon,
-    'Ajouter depuis une carte'         as title;    
+    'Ajouter'         as title
+    WHERE $tab=2;
+ 
     
 
 
--- etab_ajout_carte.sql
+++ etab_ajout_carte.sql
@@ -1,7 +1,9 @@
 SELECT 'redirect' AS component,
         'signin.sql?error' AS link
  WHERE NOT EXISTS (SELECT 1 FROM login_session WHERE id=sqlpage.cookie('session'));
+ 
 SET group_id = (SELECT user_info.groupe FROM login_session join user_info on user_info.username=login_session.username WHERE id = sqlpage.cookie('session'));
+
 SELECT 'redirect' AS component,
         'parametres.sql?restriction' AS link
         WHERE $group_id<'3';
@@ -32,36 +34,8 @@ select
     'building-community' as icon,
     'orange' as color;
     
-    -- Enregistrer la notification dans la base
--- INSERT INTO etab(type, nom_etab, description, Lat, Lon) SELECT $type, $nom_etab, $description, $Lat, $Lon WHERE $description IS NOT NULL;
- 
- -- Fiches des établissments
- SELECT 'card' as component,
-  'Établissements' AS title,
-   'Liste des écoles, collèges et lycées du PIAL' AS description;
-SELECT 
-  type  AS description,
-  'green' as color,
-  description as footer,
-  'building-community' as icon,
-  nom_etab AS title,
-  'etab_notif.sql?id=' || id as link
-FROM etab;
-/*
--- Nouvel établissement    
-SELECT 
-    'form' as component,
-    'Nouvel établissement' as title,
-    'Créer' as validate,
-    'green'           as validate_color,
-    'Recommencer'           as reset;
-    
-    SELECT 'Catégorie' AS label, 'type' AS name, 6 as width, 'select' as type, 1 as value, '[{"label": "---", "value": "---"}, {"label": "École", "value": "école"}, {"label": "Collège", "value": "Collège"}, {"label": "Lycée", "value": "Lycée"}]' as options;
-    SELECT 'Établissement scolaire' AS label, 'nom_etab' AS name, 6 as width;
-    SELECT 'Adresse' AS label, 'description' AS name;
-    SELECT 'Latitude' AS label, 'Lat' AS name, 6 as width;
-    SELECT 'Longitude' AS label, 'Lon' AS name, 6 as width;
-*/
+-- Nouvel établissement depuis une carte
+
 select 
     'form' as component,
     'GET' as method,
@@ -94,8 +68,8 @@ select
     'center' as justify
         WHERE $user_search is not Null;
 select 
-    'Ajouter' as title,
-    'etab_ajout.sql?search='||$user_search||'&lat='||$lat||'&lon='||$lon as link,
+    'Ajouter dans le formulaire' as title,
+    'etab_ajout.sql?search='||$user_search||'&lat='||$lat||'&lon='||$lon||'&tab=2' as link,
     'square-plus' as icon,
     'green' as color
         WHERE $user_search is not Null;
