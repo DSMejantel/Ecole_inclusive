316b3b7b35814588a9b96ac6dd0834385729d28d
DSMejantel <149260856+DSMejantel@users.noreply.github.com>
2024-05-30 00:00:55 +0200

    Gestion UAI et classes correspondantes

---
+++ structure/form.sql
@@ -0,0 +1,15 @@
+SELECT 'redirect' AS component,
+        'signin.sql?error' AS link
+ WHERE NOT EXISTS (SELECT 1 FROM login_session WHERE id=sqlpage.cookie('session'));
+SET group_id = (SELECT user_info.groupe FROM login_session join user_info on user_info.username=login_session.username WHERE id = sqlpage.cookie('session'));
+
+
+--- Saisir une nouvelle modalité de notification  
+SELECT 
+    'form' as component,
+    'Structure des classes' as title,
+    'Créer' as validate;
+    
+    SELECT 'select' as type, 'Établissement scolaire' AS label, 'etab' AS name, json_group_array(json_object("label", nom_etab, "value", UAI)) as options FROM etab ORDER BY nom_etab ASC;
+    SELECT 'Nom de la classe' AS label, 'classe' AS name, 'users-group' as prefix_icon, TRUE as required;
+
---
+++ structure/liste.sql
@@ -0,0 +1,22 @@
+SELECT 'redirect' AS component,
+        'signin.sql?error' AS link
+ WHERE NOT EXISTS (SELECT 1 FROM login_session WHERE id=sqlpage.cookie('session'));
+SET group_id = (SELECT user_info.groupe FROM login_session join user_info on user_info.username=login_session.username WHERE id = sqlpage.cookie('session'));
+
+-- Liste des dispositifs
+SELECT 'table' as component,
+  'Structures' AS title,
+  TRUE as small,
+  'icone' as icon;
+SELECT 
+  CASE WHEN type='Lycée'
+    THEN'building-community' 
+    WHEN type='Collège' THEN 'building-community'
+    ELSE 'building-cottage'      
+    END as icone,
+  type || ' ' || nom_etab AS Nom,
+  etab_UAI AS UAI,
+  group_concat(DISTINCT classe order by classe) as Classes
+FROM structure join etab on structure.etab_UAI=etab.UAI group by etab_UAI;
+ 
+
