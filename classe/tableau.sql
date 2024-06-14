c9e6f9589257b3efec3f59a49933f10843a8385d
DSMejantel <149260856+DSMejantel@users.noreply.github.com>
2024-06-14 15:51:13 +0200

    correction

--- classe/tableau.sql
+++ classe/tableau.sql
@@ -2,9 +2,6 @@ SELECT 'redirect' AS component,
         'signin.sql?error' AS link
  WHERE NOT EXISTS (SELECT 1 FROM login_session WHERE id=sqlpage.cookie('session'));
 SET group_id = (SELECT user_info.groupe FROM login_session join user_info on user_info.username=login_session.username WHERE id = sqlpage.cookie('session'));
-SELECT 'redirect' AS component,
-        '../parametres.sql?restriction' AS link
-        WHERE $group_id<'3';
 
 -- Calcul des variables établissement
 SET NB_eleve = (SELECT count(distinct eleve.id) FROM eleve where eleve.etab_id=$id and eleve.classe=$classe_select);
