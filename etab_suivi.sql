c2b817638a2b142c5145d7b783c7b94a964becf2
DSMejantel <149260856+DSMejantel@users.noreply.github.com>
2024-09-10 08:20:26 +0200

    correctif jointure référent

--- etab_suivi.sql
+++ etab_suivi.sql
@@ -121,7 +121,7 @@ CASE WHEN datefin>datetime(date('now')) THEN group_concat(DISTINCT modalite.type
 CASE WHEN datefin<datetime(date('now')) THEN group_concat(DISTINCT modalite.type) ELSE '-' END as droits_fermes,
 SUBSTR(referent.prenom_ens_ref, 1, 1) ||'. '||referent.nom_ens_ref as ens_ref,
 datefin as datefin
-FROM notification INNER JOIN eleve on notification.eleve_id = eleve.id LEFT join notif on notif.notification_id=notification.id LEFT join modalite on modalite.id=notif.modalite_id JOIN referent on eleve.referent_id=referent.id JOIN etab on eleve.etab_id=etab.id Where eleve.etab_id=$id group by notification.id;
+FROM notification INNER JOIN eleve on notification.eleve_id = eleve.id LEFT join notif on notif.notification_id=notification.id LEFT join modalite on modalite.id=notif.modalite_id LEFT JOIN referent on eleve.referent_id=referent.id JOIN etab on eleve.etab_id=etab.id Where eleve.etab_id=$id group by notification.id;
 
 select 
     'divider' as component,
